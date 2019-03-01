93f7f0dd2ec712757d619326c4f8a97545ef5ce4
Justin Wu <justinhwu95@gmail.com>
2019-03-01 02:04:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for(let i=0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  let i = 0
+  while(i < 4){
+    facts = facts[i] + "!!!"
+    console.log(--i)
+  }
+  return facts
+}
